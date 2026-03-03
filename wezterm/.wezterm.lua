9d9df2e24e6a3a86d6110181e85f2013bc4ba5da
Andrew Wu <andrew_wu@morrisonexpress.com>
2026-03-03 15:43:17 +0800

    feature(Wezterm): adjust initial height and width

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
 config.initial_rows = 50
-config.initial_cols = 180
+config.initial_cols = 200
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
